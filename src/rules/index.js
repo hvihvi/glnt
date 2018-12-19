9694d2746b259ecd3d3bcbb2bc62e97852f443be
hvi <hvi@lesfurets.com>
2018-12-19 17:17:23 +0100

    Setup eslint and prettier

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,7 +1,8 @@
 const { onEachCommit, onEachCommitPatches } = require("../git/index");
 const config = require("../config/index").config;
 const shouldHaveTests = require("./shouldHaveTests").shouldHaveTests;
-const shouldHaveFormattedMessage = require("./shouldHaveFormattedMessage").shouldHaveFormattedMessage;
+const shouldHaveFormattedMessage = require("./shouldHaveFormattedMessage")
+  .shouldHaveFormattedMessage;
 
 const applyRules = () => {
   console.log("[DEBUG] Applying rules");
