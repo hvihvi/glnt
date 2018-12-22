6733f11f720dd54795b076196d379d21ae8e0524
hvi <hvi@lesfurets.com>
2018-12-22 18:46:42 +0100

    Refactor : named import

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,4 +1,4 @@
-const { onEachCommit, onEachCommitPatches } = require("../git/index");
+const git = require("../git/index");
 const config = require("../config/index").config;
 const shouldHaveTests = require("./shouldHaveTests").shouldHaveTests;
 const shouldHaveFormattedMessage = require("./shouldHaveFormattedMessage")
@@ -7,10 +7,10 @@ const shouldHaveFormattedMessage = require("./shouldHaveFormattedMessage")
 const applyRules = () => {
   console.log("[DEBUG] Applying rules");
   if (config.shouldHaveTests.enabled) {
-    onEachCommitPatches(shouldHaveTests);
+    git.onEachCommitPatches(shouldHaveTests);
   }
   if (config.shouldHaveFormattedMessage.enabled) {
-    onEachCommit(shouldHaveFormattedMessage);
+    git.onEachCommit(shouldHaveFormattedMessage);
   }
 };
 
