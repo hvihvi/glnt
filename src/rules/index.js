16d9bbd3046e0011068e4f82fc1c7601b93cc763
hvi <hvi@lesfurets.com>
2018-12-24 17:57:45 +0100

    Re-enable should have test rule

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,19 +1,17 @@
 const git = require("../git/index");
 const config = require("../config/index").config;
-// const shouldHaveTests = require("./shouldHaveTests").shouldHaveTests;
+const shouldHaveTests = require("./shouldHaveTests").shouldHaveTests;
 const shouldHaveFormattedMessage = require("./shouldHaveFormattedMessage")
   .shouldHaveFormattedMessage;
 
 const applyRules = async () => {
-  const ancestor = await git.findCommonAncestor("HEAD", config.origin);
-  console.log("[DEBUG] ancestor:" + ancestor);
-  const commits = await git.listCommits(ancestor, "HEAD");
-  console.log("[DEBUG] commit list:" + commits);
+  const base = await git.findCommonAncestor("HEAD", config.origin);
+  const commits = await git.listCommits(base, "HEAD");
 
   commits.forEach(commit => applyCommitRules(commit));
-  // if (config.shouldHaveTests.enabled) {
-  //   git.onEachCommitPatches(shouldHaveTests);
-  // }
+  if (config.shouldHaveTests.enabled) {
+    git.onEachCommitPatches(shouldHaveTests);
+  }
 };
 
 const applyCommitRules = commit => {
