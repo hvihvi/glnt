7aa20614c65ddce1cabf6424da17d13774139188
hvi <hvi@lesfurets.com>
2019-02-02 23:39:09 +0100

    Add test with empty list

--- src/rules/shouldNotBeUsedByOthers.test.ts
+++ src/rules/shouldNotBeUsedByOthers.test.ts
@@ -18,3 +18,23 @@ it("should filter origin and ignores branches", () => {
   // then
   expect(usrs).toEqual(["origin/test3", "origin/test4"]);
 });
+
+it("should filter origin with empty ignores branches", () => {
+  // given
+  const isUsedBy = [
+    "origin/test1",
+    "origin/test2",
+    "origin/test3",
+    "origin/test4",
+    "origin/master"
+  ];
+  // when
+  const usrs = users(isUsedBy, [], "origin/master");
+  // then
+  expect(usrs).toEqual([
+    "origin/test1",
+    "origin/test2",
+    "origin/test3",
+    "origin/test4"
+  ]);
+});
